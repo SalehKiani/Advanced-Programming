ebe23922a227dd0ebac32bef59244a9cf6a1e7ce
Saleh Kiani <kiani.mohammadsaleh@gmail.com>
2019-03-26 14:22:00 +0430

    HW1 2nd section

--- Program.cs
+++ Program.cs
@@ -10,17 +10,7 @@ namespace HW1_SalehKiani_972164024
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("please enter your 10 numbers");
-            int a = new int();
-            int max=new int();
-            max = 0;
-            for(int b=0;b<10;b++)
-            {
-                a = Int32.Parse(Console.ReadLine());
-                if (a > max)
-                    max = a;
-            }
-            Console.WriteLine("The Biggest Number is {0}",Convert.ToString(max));
+            Console.WriteLine(DateTime.Now.ToString("M/d/yyyy"));
             Console.ReadLine();
         }
     }
